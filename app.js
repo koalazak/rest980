d256f05c1121dec71f0d8ea2c52046f3d38ae886
Allan Glen <allan@allanglen.com>
2016-11-30 22:56:07 -0700

    Add support for HTTP basic auth

--- app.js
+++ app.js
@@ -4,11 +4,41 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var basicAuth = require('basic-auth')
+var config = require('config');
+
 var helloRoute = require('./routes/index');
 var apiRoute = require('./routes/api');
 var mapRoute = require('./routes/map');
 
 var app = express();
+
+// Authentication handler
+var basicAuthUser = process.env.BASIC_AUTH_USER || config.basicAuthUser;
+var basicAuthPass = process.env.BASIC_AUTH_PASS || config.basicAuthPass;
+var authHandler = function (req, res, next) {
+  if (!basicAuthUser || !basicAuthPass) {
+    return next();
+  }
+
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.send(401);
+  };
+
+  // Get basis auth credentials
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  // Check credentials
+  if (user.name === basicAuthUser && user.pass === basicAuthPass) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,9 +48,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', helloRoute);
-app.use('/api', apiRoute);
-app.use('/map', mapRoute);
+app.use('/', authHandler, helloRoute);
+app.use('/api', authHandler, apiRoute);
+app.use('/map', authHandler, mapRoute);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
